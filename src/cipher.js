69796018c4df19622734c86545d847accdf0717d
javieramontenegro <doki.snsd@gmail.com>
2019-04-05 12:57:37 -0400

    cifrado y descifrado

--- src/cipher.js
+++ src/cipher.js
@@ -1,28 +1,52 @@
 window.cipher = {
   encode: (offset,input) => {
     /* Acá va tu código */
-    //let input = document.getElementById("input");
-    //let output = document.getElementById("output");
-    //let des=  document.getElementById("offsetcifrado");
-  
      let output ="";
-   for(let i=0; i<input.length; i++){
-       let ascii= input.charCodeAt(i);
-       if(ascii>=65 && ascii<=90 ){
+//BUCLE QUE LEE CADA LETRA   
+     for(let i=0; i<input.length; i++){
+      // TRANSFORMA A NUMERO ASCII
+      let ascii= input.charCodeAt(i);
+      //CASO MAYUSSCULAS
+      if(ascii>=65 && ascii<=90 ){
         let n= parseInt(offset);
         output +=String.fromCharCode((ascii - 65 + n) % 26 + 65)
-        console.log(output);
+        
       }
+      // CASO MINUSCULAS  
+      else if(ascii >= 97 &&  ascii<= 122){
+        let n= parseInt(offset);
+        output += String.fromCharCode((ascii - 97 + n) % 26 + 97);
+}
    
-   }
-    
-      
-   return output;
+   //ESPACIO Y LO DEMAS
+ else {output+= input.charAt(i)};
    
+    }
+
+ return output;
    
-   
-  },
-  decode: () => {
+     },
+  decode: (offset,input) => {
     /* Acá va tu código */
-  }
+ let outputdes ="";
+ for(let i=0; i<input.length; i++){
+     let ascii= input.charCodeAt(i);
+     if(ascii>=65 && ascii<=90 ){
+      let n= parseInt((26-offset)%26);
+      outputdes +=String.fromCharCode((ascii-65+n)%26 +65);
+      
+    }
+    else if(ascii >= 97 &&  ascii<= 122){
+      let n= parseInt((26-offset)%26);
+      outputdes += String.fromCharCode((ascii - 97 + n) % 26 + 97);
+}
+ 
+ else {outputdes+= input.charAt(i)};
+ }
+return outputdes;
+
+}
+
+
+
 };
--- src/index.js
+++ src/index.js
@@ -1,12 +1,15 @@
-let input = document.getElementById("input").value;
-let output = document.getElementById("output").value;
-let offset=  document.getElementById("offsetcifrado").value;
+//let input = document.getElementById("input").value;
+//let output = document.getElementById("output").value;
+//let offset=  document.getElementById("offsetcifrado").value;
 
+//BOTON PARA CIFRAR
 let btnCifrar= document.getElementById("cifrar");
 btnCifrar.addEventListener('click',()=>{
     document.getElementById("output").innerHTML = window.cipher.encode(document.getElementById("offsetcifrado").value,document.getElementById("input").value);
-})
+});
 
-//document.getElementById("cifrar")= window.cipher.encode(offset,input);
-
-//btnCifrar.addEventListener('click',()=>{window.cipher(document.getElementById("cifrar")= window.cipher.encode(offset,input)) })
+//BOTON PARA DESCIFRAR
+let btnDescifrar = document.getElementById("descifrar");
+btnDescifrar.addEventListener('click',()=>{
+    document.getElementById("desoutput").innerHTML = window.cipher.decode(document.getElementById("offsetdescifrado").value,document.getElementById("inputdescifrar").value);
+} );
